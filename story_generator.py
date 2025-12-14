469ac67e233f39a228962475ab1c4e795b2c7061
Lakshita Mukati <102685551+LAKSHITAAAAA@users.noreply.github.com>
2025-12-15 00:33:29 +0530

    Create story_generator.py

---
+++ story_generator.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import random
+
+# Path to your Sherlock Holmes text files (relative to this script)
+story_path = "../data/sherlock/"
+
+# Function to read all text files from the folder
+def read_all_stories(story_path):
+    txt = []
+    for _, _, files in os.walk(story_path):
+        for file in files:
+            with open(story_path + file, 'r', encoding='utf-8') as f:
+                for line in f:
+                    line = line.strip()
+                    if line == '----------':
+                        break
+                    if line != '':
+                        txt.append(line)
+    return txt
+
+# Read all stories
+stories = read_all_stories(story_path)
+print("number of lines = ", len(stories))
+
+# Function to clean and tokenize text
+def clean_txt(txt):
+    cleaned_txt = []
+    for line in txt:
+        line = line.lower()
+        line = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-\\]", "", line)
+        tokens = word_tokenize(line)
+        words = [word for word in tokens if word.isalpha()]
+        cleaned_txt += words
+    return cleaned_txt
+
+# Clean all stories
+cleaned_stories = clean_txt(stories)
+print("number of words = ", len(cleaned_stories))
+
+# Function to create an n-gram Markov model
+def make_markov_model(cleaned_stories, n_gram=2):
+    markov_model = {}
+    for i in range(len(cleaned_stories) - n_gram - 1):
+        curr_state, next_state = "", ""
+        for j in range(n_gram):
+            curr_state += cleaned_stories[i + j] + " "
+            next_state += cleaned_stories[i + j + n_gram] + " "
+        curr_state = curr_state[:-1]
+        next_state = next_state[:-1]
+        if curr_state not in markov_model:
+            markov_model[curr_state] = {}
+            markov_model[curr_state][next_state] = 1
+        else:
+            if next_state in markov_model[curr_state]:
+                markov_model[curr_state][next_state] += 1
+            else:
+                markov_model[curr_state][next_state] = 1
+
+    # Convert counts to probabilities
+    for curr_state, transition in markov_model.items():
+        total = sum(transition.values())
+        for state, count in transition.items():
+            markov_model[curr_state][state] = count / total
+
+    return markov_model
+
+# Build the Markov model
+markov_model = make_markov_model(cleaned_stories)
+print("number of states = ", len(markov_model.keys()))
+print("All possible transitions from 'the game' state: \n")
+print(markov_model.get('the game', {}))
+
+# Function to generate story text from Markov model
+def generate_story(markov_model, limit=100, start='my god'):
+    n = 0
+    curr_state = start
+    story = curr_state + " "
+    while n < limit:
+        if curr_state not in markov_model:
+            break
+        next_state = random.choices(
+            list(markov_model[curr_state].keys()),
+            list(markov_model[curr_state].values())
+        )
+        curr_state = next_state[0]
+        story += curr_state + " "
+        n += 1
+    return story
+
+# Generate 20 example story snippets
+for i in range(20):
+    print(str(i) + ". ", generate_story(markov_model, start="dear holmes", limit=8))
